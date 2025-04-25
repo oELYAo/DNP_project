3752d6c01c915db44242601ac38e0b7ff4fd4ca2
stepan <step.way.a12@gmail.com>
2025-04-25 17:33:55 +0300

    chore: wraped mapper_wordcount and reducer_wordcount in a function

--- src/wordcount/mapper_wordcount.py
+++ src/wordcount/mapper_wordcount.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-import sys
-for line in sys.stdin:
-    for token in line.strip().split():
-        print(f"{token}\t1")
+def map_wordcount(input_stream):
+    for line in input_stream:
+        for token in line.strip().split():
+            print(f"{token}\t1")
+
+if __name__ == "__main__":
+    import sys
+    map_wordcount(sys.stdin)
