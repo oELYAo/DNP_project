ea4c4c1df9e10e64c91ad7500ebfe0dc069bab27
ub3rch <v.molchanov@innopolis.university>
2025-04-27 15:03:18 +0300

    Feature: Add unit testing, small improvements in CI pipeline

---
+++ tests/test_wordcount/test_reducer.py
@@ -0,0 +1,23 @@
+from src.wordcount.mapper import mapper_wordcount
+
+class TestReducerWordcout:
+    def test_empty(self):
+        input = []
+        output = mapper_wordcount(input)
+        assert not output
+
+    def test_single_string(self):
+        input = ['usual\t1', 'single\t1', 'input\t1', 'string\t1']
+        truth = {'usual': 1, 'single': 1, 'input': 1, 'string': 1}
+        output = mapper_wordcount(input)
+        assert len(truth) == len(output)
+        for key in truth:
+            assert truth[key] == output[key]
+
+    def test_repeating(self):
+        input = ['test\t1', 'test\t1', 'test\t1', 'test\t1', 'test\t1']
+        truth = {'test': 5}
+        output = mapper_wordcount(input)
+        assert len(truth) == len(output)
+        for key in truth:
+            assert truth[key] == output[key]
