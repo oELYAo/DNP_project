a3ef6ad8d5fa60f8bc3d7d2e8979ac2d2445dc38
ub3rch <v.molchanov@innopolis.university>
2025-04-29 16:18:13 +0300

    Rename sentiment and wordcount files. Some changes in test.

--- tests/test_wordcount/test_mapper.py
+++ tests/test_wordcount/test_mapper.py
@@ -1,16 +1,15 @@
-import sys
-from src.wordcount.mapper import mapper_wordcount
+from src.wordcount.mapper import mapper
 
 class TestMapperWordcount:
     def test_empty(self):
         input = ''
-        output = mapper_wordcount(input)
+        output = mapper(input)
         assert not output
 
     def test_single_string(self):
         input = 'usual single input string'
         truth = ['usual\t1', 'single\t1', 'input\t1', 'string\t1']
-        output = mapper_wordcount(input)
+        output = mapper(input)
         assert len(truth) == len(output)
         for i in range(len(truth)):
             assert truth[i] == output[i]
@@ -18,7 +17,7 @@ class TestMapperWordcount:
     def test_repeating(self):
         input = 'test test test test test'
         truth = ['test\t1', 'test\t1', 'test\t1', 'test\t1', 'test\t1']
-        output = mapper_wordcount(input)
+        output = mapper(input)
         assert len(truth) == len(output)
         for i in range(len(truth)):
             assert truth[i] == output[i]
